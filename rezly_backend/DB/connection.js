21cbd830e1a873082ebebd64f41f6feb1afd6496
Tasbeeh Salahat <tasbeehsa80@gmail.com>
2025-10-19 18:08:50 +0300

    Delete Nember

--- rezly_backend/DB/connection.js
+++ rezly_backend/DB/connection.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected successfully");
   } catch (err) {
